0b66ace1ca80c85ef968c563290a7b39482c8ab6
stef <stefan.manea198@gmail.com>
2022-10-11 16:53:10 +0300

    add h1, starting point

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      // user1:{username:'stefan', password:'ilikedogs', authorized:false}
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1>Enter the password</h1>
+        <form action='#'>
+            <input type='text' placeholder='username'/>
+            <input type='password' placeholder='password'/>
+            <button>Sign in</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
