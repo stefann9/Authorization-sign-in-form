6432a4678335c74484739cbbf3d3c954e30d501d
stef <stefan.manea198@gmail.com>
2022-10-11 17:48:12 +0300

    add form functionality authorization

--- src/App.js
+++ src/App.js
@@ -8,14 +8,19 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state={
-      user1:{username:'stefan', password:'ilikedogs', authorized:false}
+      user1:{username:'stefan', password:'ilikedogs' },
+      authorized:false
     }
   }
+  handlerFormData = (data)=>{
+    this.setState({authorized:data})
+  }
+
   render(){
     return (
       <div className="App">
-        <h1>Enter the password</h1>
-        <FormAuth user1={this.state.user1}/>
+        <h1>Enter the password {this.state.authorized && 'dogs'}</h1>
+        <FormAuth user1={this.state.user1} onSubmit={this.handlerFormData}/>
       </div>
     );
   }
--- src/components/form-auth/form-auth.component.jsx
+++ src/components/form-auth/form-auth.component.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
 export class FormAuth extends React.Component {
+  onSubmit = (e) => {
+    const [usernameInput, passwordInput] = e.target.querySelectorAll("input");
+    const { username: usernameProp, password: passwordProp } = this.props.user1;
+    let authorized;
+    console.log(this.props)
+    if (
+      passwordProp === passwordInput.value &&
+      usernameProp === usernameInput.value
+    ) {
+      authorized = true;
+    } else {
+      authorized = false;
+    }
+    this.props.onSubmit(authorized);
+  };
   render() {
     return (
-      <form action="#">
+      <form action="#" onSubmit={this.onSubmit}>
         <input type="text" placeholder="username" />
         <input type="password" placeholder="password" />
         <button>Sign in</button>
